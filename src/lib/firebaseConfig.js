1a2513c542b0aacf088b45fd01d70a858992fcba
ldmora13 <moraparadaluisdavid@gmail.com>
2025-04-14 14:37:01 -0500

    Firebase insert 🧯

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDK4Q3BHWw7euBCA0TfbOVEfQZ0O6sps-k",
+  authDomain: "jerseys-colombia-31c9f.firebaseapp.com",
+  projectId: "jerseys-colombia-31c9f",
+  storageBucket: "jerseys-colombia-31c9f.firebasestorage.app",
+  messagingSenderId: "806324211210",
+  appId: "1:806324211210:web:af031f3f1a4bd368ee59f3",
+  measurementId: "G-EGNWNK7B21"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,38 @@
 import React, { useEffect, useState, useRef } from "react";
 import logo from "../assets/football-jersey.svg";
 import googlelogo from "../assets/google.svg";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, provider } from "../lib/firebaseConfig"; 
+
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      console.log("Login exitoso:", user);
+      // Redireccionar o actualizar estado global aquí
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error.message);
+      setError("Correo o contraseña incorrectos.");
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      console.log("Usuario logueado con Google:", user);
+    } catch (error) {
+      console.error("Error en login con Google", error);
+    }
+  };
 
   const [dropdownVisible, setDropdownVisible] = useState(false);
   const dropdownRef = useRef(null);
@@ -74,7 +104,7 @@ const Login = () => {
                         <path d="m15.626 11.769a6 6 0 1 0 -7.252 0 9.008 9.008 0 0 0 -5.374 8.231 3 3 0 0 0 3 3h12a3 3 0 0 0 3-3 9.008 9.008 0 0 0 -5.374-8.231zm-7.626-4.769a4 4 0 1 1 4 4 4 4 0 0 1 -4-4zm10 14h-12a1 1 0 0 1 -1-1 7 7 0 0 1 14 0 1 1 0 0 1 -1 1z"></path>
                       </g>
                     </svg>
-                    <p>Login</p>
+                    <p id="text-login">Login</p>
                   </div>
                 </div>
 
@@ -93,7 +123,7 @@ const Login = () => {
                             <svg className="h-[1.3em] w-[1.3em] fill-white" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                               <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z" />
                             </svg>
-                            <input id="email" className="bg-transparent border-none outline-none w-full text-[#d3d3d3]" type="email" placeholder="Email" autoComplete="off"/>
+                            <input id="email" className="bg-transparent border-none outline-none w-full text-[#d3d3d3]" type="email" placeholder="Email" autoComplete="off" value={email} onChange={(e) => setEmail(e.target.value)}/>
                           </div>
 
                           {/* Password input */}
@@ -101,22 +131,20 @@ const Login = () => {
                             <svg className="h-[1.3em] w-[1.3em] fill-white" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                               <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
                             </svg>
-                            <input id="password" className="bg-transparent border-none outline-none w-full text-[#d3d3d3]" type="password" placeholder="Password" autoComplete="off"/>
+                            <input id="password" className="bg-transparent border-none outline-none w-full text-[#d3d3d3]" type="password" placeholder="Password" autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} />
                           </div>
 
                           {/* Buttons */}
                           <div className="flex justify-around text-white">
-                            <button id="btn-login" className="p-2 px-4 rounded-md bg-[#252525] hover:bg-black transition">
+                            <button onClick={handleLogin} className="p-2 px-4 rounded-md bg-[#252525] hover:bg-black transition">
                               Iniciar sesión
                             </button>
-                            <button
-                              id="btn-google"
-                              className="p-2 px-4 rounded-md bg-[#252525] text-white hover:bg-black transition">
+                            <button onClick={handleGoogleLogin} id="btn-google" className="p-2 px-4 rounded-md bg-[#252525] text-white hover:bg-black transition">
                               <img src={googlelogo} className="w-[40px]" alt="Google login"/>
                             </button>
-                            
                           </div>
-                          <a href="/register" id="btn-sign" className=" text-center p-2 rounded-md bg-[#252525] text-white hover:bg-[#AFFCBE] hover:text-black">
+                          {error && <p className="text-red-600 text-sm text-center">{error}</p>} 
+                          <a href="/register" id="btn-sign" className="text-center p-2 rounded-md bg-[#252525] text-white hover:bg-[#AFFCBE] hover:text-black">
                             Registrarse
                           </a>
                           <button className="p-2 rounded-md bg-[#252525] text-white hover:bg-[#DA544A] hover:text-black">

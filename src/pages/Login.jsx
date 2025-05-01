75ed77cd87ff70912393e2e57570e4864010e0fe
ldmora13 <moraparadaluisdavid@gmail.com>
2025-04-30 22:30:54 -0500

    Scroller Button ⏬

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,8 @@ import Loader from "../components/Loader.jsx"
 import {supabase} from "../lib/supabaseClient"
 import logo from "../assets/football-jersey.svg";
 import googlelogo from "../assets/google.svg";
+import { ChevronDown, ChevronUp } from 'lucide-react';
+
 
 const BASE_URL = 'https://nnungauvdtershilojxj.supabase.co/storage/v1/object/public/camisetas-futbol/'
 
@@ -40,6 +42,39 @@ const Login = () => {
   const [hoveredIndexFutbol, setHoveredIndexFutbol] = useState(null);
   const [hoveredIndexNBA, setHoveredIndexNBA] = useState(null);
   const [hoveredIndexF1, setHoveredIndexF1] = useState(null);
+
+  {/* Preguntas frecuentes*/}
+  const [isAtTop, setIsAtTop] = useState(false);
+  const topRef = useRef(null);
+  const preguntasRef = useRef(null);
+  const [activa, setActiva] = useState(null);
+  const [enPreguntas, setEnPreguntas] = useState(false);
+  const preguntas = [
+    {
+      pregunta: '¿Cuanto tiempo demora en llegar mi pedido?',
+      respuesta: 'El tiempo estimado de exportacion es de 2 días, el tiempo de envio depende del país de destino, pero en general es de 15 a 20 días hábiles.'
+    },
+    {
+      pregunta: '¿Como se mi talla?',
+      respuesta: 'Las tallas son las estandarizadas por el sistema internacional, por lo que puedes consultar la tabla de tallas en cada producto.'
+    },
+    {
+      pregunta: '¿Puedo devolver mi pedido?',
+      respuesta: 'Si, puedes devolver tu pedido en un plazo de 15 días desde la fecha de entrega. Para más información, consulta nuestra política de devoluciones.'
+    },
+    {
+      pregunta: '¿Como puedo pagar?',
+      respuesta: 'Aceptamos tarjetas de crédito y débito, PayPal y transferencias bancarias y todo, a travez de portales totalmente seguros y estandarizados. Puedes elegir tu método de pago preferido al finalizar la compra.'
+    },
+    {
+      pregunta: '¿Como puedo saber donde está mi pedido?',
+      respuesta: 'Una vez que tu pedido haya sido enviado, recibirás un correo electrónico con un enlace de seguimiento. Puedes usar ese enlace para ver el estado de tu pedido en tiempo real o si lo prefieres, puedes consultar el estado de tu pedido en la sección "Compras" de tu cuenta.'
+    }
+
+  ]
+  const togglePregunta = (index) => {
+    setActiva(activa === index ? null : index);
+  };
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -97,59 +132,67 @@ const Login = () => {
 
 
   useEffect(() => {
-    const fetchCamisetasFutbol = async () => {
-      const { data, error } = await supabase  
-        .from('selecciones')
-        .select('nombre, imagenes, pais, año, fecha_indexacion')
-        .order('año', { ascending: true })
-        .limit(3)
-      if (error) {
-        console.error('Error al traer camisetas:', error)
-      } else {
-        setCamisetasFutbol(data)
-      } 
-      
-    }
-
-    fetchCamisetasFutbol()
-  }, [])
-
-  useEffect(() => {
-    const fetchCamisetasNBA = async () => {
-      const { data, error } = await supabase
-        .from('NBA-1')
-        .select('nombre, imagenes, team, año, fecha_indexacion, player')
-        .order('año', { ascending: true })
-        .limit(3)
-      if (error) {
-        console.error('Error al traer camisetas:', error)
-      } else {
-        setCamisetasNBA(data)
-      } 
-    }
-
-    fetchCamisetasNBA()
-  }, [])
-
-  useEffect(() => {
-    const fetchCamisetasF1 = async () => {
-      const { data, error } = await supabase
-        .from('F1')
-        .select('nombre, imagenes, team, año, fecha_indexacion, driver')
-        .order('año', { ascending: true })
-        .limit(3)
-      if (error) {
-        console.error('Error al traer camisetas:', error)
-      } else {
-        setCamisetasF1(data)
+    const fetchCamisetas = async () => {
+      try {
+        const [futbolRes, nbaRes, f1Res] = await Promise.all([
+          supabase
+            .from('selecciones')
+            .select('nombre, imagenes, pais, año, fecha_indexacion')
+            .order('año', { ascending: true })
+            .limit(3),
+  
+          supabase
+            .from('NBA-1')
+            .select('nombre, imagenes, team, año, fecha_indexacion, player')
+            .order('año', { ascending: true })
+            .limit(3),
+  
+          supabase
+            .from('F1')
+            .select('nombre, imagenes, team, año, fecha_indexacion, driver')
+            .order('año', { ascending: true })
+            .limit(3)
+        ])
+  
+        if (futbolRes.error) console.error('Error en Fútbol:', futbolRes.error)
+        else setCamisetasFutbol(futbolRes.data)
+  
+        if (nbaRes.error) console.error('Error en NBA:', nbaRes.error)
+        else setCamisetasNBA(nbaRes.data)
+  
+        if (f1Res.error) console.error('Error en F1:', f1Res.error)
+        else setCamisetasF1(f1Res.data)
+  
+      } catch (err) {
+        console.error('Error al cargar camisetas:', err)
+      } finally {
+        // Espera opcional antes de quitar el loader
+        setTimeout(() => {
+          setLoading(false)
+        }, 1500)
       }
-      setTimeout(() => {
-        setLoading(false);
-      }, 2000); 
     }
-
-    fetchCamisetasF1()
+  
+    fetchCamisetas()
   }, [])
+  
+  const handleScroll = () => {
+    if (isAtTop) {
+      topRef.current.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      preguntasRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+    setIsAtTop(!isAtTop);
+  };
+  useEffect(() => {
+    const handleScrollPosition = () => {
+      const scrollY = window.scrollY;
+      setIsAtTop(scrollY > 100); 
+    };
+    window.addEventListener("scroll", handleScrollPosition);
+    return () => window.removeEventListener("scroll", handleScrollPosition);
+  }, []);
+
 
 
   return (
@@ -161,7 +204,7 @@ const Login = () => {
         </div>
       )}
 
-      <header className="bg-[#E8E8E8] p-4 relative w-full z-[1000] top-0 left-0 h-[70px] border-b-[2px] border-transparent">
+      <header ref={topRef} className="bg-[#E8E8E8] p-4 relative w-full z-[1000] top-0 left-0 h-[70px] border-b-[2px] border-transparent">
         <div className="max-w-[1200px] mx-auto px-5 flex items-center justify-between h-full">
           
           {/* Logo */}
@@ -308,8 +351,8 @@ const Login = () => {
         <div className="absolute bottom-0 left-0 w-full h-[2px] bg-gradient-to-r from-transparent via-[#00ff2a] to-transparent">
         </div>
       </header>
-      <main className="bg-[#E8E8E8] flex items-center justify-start h-full w-full p-[20px]">
-        <div className="flex flex-row justify-center items-start gap-10 w-full">
+      <main className="bg-[#E8E8E8] flex items-center justify-start h-full w-full p-[20px] flex-col">
+        <div className="mt-2 flex flex-row justify-center items-start gap-10 w-full">
 
           {/* Contenedor 1 */}
           <div onClick={() => navigate("/futbol")} className="group flex flex-col items-center justify-center  w-[300px] h-[400px] rounded-2xl bg-[#e0e0e0]"
@@ -381,7 +424,7 @@ const Login = () => {
             <div className="flex flex-col gap-4 overflow-visible">
               {camisetasF1.map((camiseta, index) => {
                 const imagenes = camiseta.imagenes || []
-                const imagenPrincipal = imagenes.length > 0 ? imagenes[imagenes.length - 2] : null
+                const imagenPrincipal = imagenes.length > 0 ? imagenes[imagenes.length - 1] : null
 
                 return (
                   <div key={index} onMouseEnter={() => setHoveredIndexF1(index)} onMouseLeave={() => setHoveredIndexF1(null)}
@@ -393,7 +436,7 @@ const Login = () => {
                       <img
                         src={imagenPrincipal}
                         alt={camiseta.nombre}
-                        className="mx-2 w-[80px] h-[80px] object-cover rounded"
+                        className="w-[80px] h-[80px] object-contain rounded"
                       />
                     )}
                     <div className="flex flex-col">
@@ -403,6 +446,45 @@ const Login = () => {
                   </div>
                 )
               })}
+            </div>
+          </div>
+          
+        </div>
+        <button
+          onClick={handleScroll}
+          type="button"
+          className={`fixed ${
+            isAtTop ? "top-5" : "bottom-5"
+          } left-1/2 transform -translate-x-1/2 cursor-pointer z-10 px-6 py-2 overflow-hidden border-2 border-gray-200 rounded-full bg-gray-50 text-gray-800 text-lg font-semibold group shadow-xl
+            before:absolute before:w-0 before:h-full before:top-0 before:left-0 before:bg-emerald-500 before:transition-all before:duration-500 before:rounded-full
+            hover:before:w-full hover:text-white`}
+        >
+          <span className="relative z-10 flex items-center gap-2">
+            {isAtTop ? "↑ Volver" : "Explorar ↓"}
+          </span>
+        </button>
+        <div className="h-screen flex items-center justify-center relative flex-col">
+
+          {/* Sección de preguntas frecuentes */}
+          <div className="py-16">
+            <h1 className="mt-50 text-2xl font-bold text-center mb-6">Preguntas frecuentes</h1>
+            <div ref={preguntasRef} className="max-w-xl mx-auto p-4 space-y-4">
+              {preguntas.map((item, index) => (
+                <div key={index} className="border rounded-lg shadow-sm">
+                  <button
+                    onClick={() => togglePregunta(index)}
+                    className="w-full flex justify-between items-center p-4 text-left font-medium"
+                  >
+                    {item.pregunta}
+                    {activa === index ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
+                  </button>
+                  {activa === index && (
+                    <div className="px-4 pb-4 text-gray-700 animate-fade-in">
+                      {item.respuesta}
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
         </div>

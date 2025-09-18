b773e6c39179c0e399fc58cdd1f8550ca750347a
ldmora <moraparadaluisdavid@gmail.com>
2025-09-18 12:53:29 -0500

    ✅ Supabase fixed

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import {getAuth} from 'firebase/auth'
 
-const supabaseUrl = 'https://ncelywzhnnvmciefvjfi.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 
